2c607c71d3745fd0246a0e742a6530038487bcba
Tom Ritter <tom@ritter.vg>
2017-02-01 10:51:46 -0600

    We do not need a special docker container for this.

--- build-image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -vex
-
-apt-get update
-apt-get install -y python python-requests python-feedparser git curl wget
-
-cd /home/worker/workspace
-git clone https://github.com/mozilla-services/third-party-library-alert.git
