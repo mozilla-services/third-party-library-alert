4b1293b14fcbdcf820ecb89b13e490214931b5b7
Tom Ritter <tom@ritter.vg>
2017-02-10 11:53:16 -0600

    Refactor heavily to allow for different fetching types. Add sqlite (which needs a new fetching type)

--- check.py
+++ check.py
@@ -14,21 +14,45 @@ UPDATE = 1
 AHEAD = 2
 
 
-def get_mozilla_version(config):
-	t = requests.get(config['current_version_file'])
-	m = re.search(config['current_version_re'], t.text)
-	if m:
-		current_version = m.groups(0)[0]
-		if config['verbose']:
-			print "\tFound mozilla version", current_version
-		return current_version 
-	else:
-		raise Exception("Could not match the regular expression '" + str(config['current_version_re']) + "' in the text\n\n" + str(t.text))
+################################################################################
+
+def validate_config(config):
+	if config['latest_version_fetch_type'] == 'github_rss' and \
+	   not config['latest_version_fetch_location'].endswith('/'):
+		config['latest_version_fetch_location'] += '/'
+
+	if not config['current_version_fetch_location'].startswith('https://hg.mozilla.org/mozilla-central/raw-file/tip/'):
+		raise Exception("current_version_fetch_location (" + config['current_version_fetch_location'] + ") does not appear to be a hg.mozilla link.")
+
+	return config
 
 ################################################################################
 
-def _latest_version_release_version(config):
-	doc = feedparser.parse(config['repo_url'] + "releases.atom")
+def _current_version_hg_re(config):
+	t = requests.get(config['current_version_fetch_location'])
+	m = re.search(config['current_version_re'], t.text)
+	if m:
+		current_version = m.groups(0)[0]
+		return current_version 
+	else:
+		raise Exception("Could not match the regular expression '" + str(config['current_version_re']) + "' in the text\n\n" + str(t.text))	
+
+def get_mozilla_version(config):
+	if config['current_version_fetch_type'] == 'hg.moz_re':
+		current_version = _current_version_hg_re(config)
+	else:
+		raise Exception("Received an unknown current_version_fetch_type: " + str(config['current_version_fetch_type']))
+
+	if config['verbose']:
+		print "\tFound mozilla version", current_version
+
+	return current_version
+	
+
+################################################################################
+
+def _latest_version_github_rss(config):
+	doc = feedparser.parse(config['latest_version_fetch_location'] + "releases.atom")
 
 	if len(doc['entries']) < 1:
 		raise Exception("No entries were found at the atom url")
@@ -36,27 +60,29 @@ def _latest_version_release_version(config):
 	latest_version = doc['entries'][0]['link']
 
 	#Clean up
-	latest_version = latest_version.replace(config['repo_url'] + "releases/tag/", "")
+	latest_version = latest_version.replace(config['latest_version_fetch_location'] + "releases/tag/", "")
 	if latest_version[0] == 'v':
 		latest_version = latest_version[1:]
 
 	return latest_version
 
-def _latest_version_git_commit(config):
+def _latest_version_multiline_html_re(config):
+	t = requests.get(config['latest_version_fetch_location'])
+	m = re.search(config['latest_version_re'], t.text, re.MULTILINE)
+	if m:
+		latest_version = m.groups(0)[0]
+		return latest_version 
+	else:
+		raise Exception("Could not match the regular expression '" + str(config['latest_version_re']) + "' in the text\n\n" + str(t.text))	
 	return latest_version
 
-################################################################################
-
 def get_latest_version(config):
-	if not config['repo_url'].endswith('/'):
-		config['repo_url'] += '/'
-
-	if config['compare_type'] == 'release_version':
-		latest_version = _latest_version_release_version(config)
-	elif config['compare_type'] == 'git_commit':
-		latest_version = _latest_version_git_commit(config)
+	if config['latest_version_fetch_type'] == 'github_rss':
+		latest_version = _latest_version_github_rss(config)
+	elif config['latest_version_fetch_type'] == 'multiline_html_re':
+		latest_version = _latest_version_multiline_html_re(config)
 	else:
-		raise Exception("Received an unknown comparison type: " + str(config['compare_type']))
+		raise Exception("Received an unknown latest_version_fetch_type: " + str(config['latest_version_fetch_type']))
 
 	if config['verbose']:
 		print "\tFound version", latest_version
@@ -85,53 +111,79 @@ def check_version(config, current_version, latest_version):
 
 LIBRARIES = [
 	{
+		'title' : 'sqlite',
+		'location' : 'db/sqlite3',
+
+		'latest_version_fetch_type' : 'multiline_html_re',
+		'latest_version_fetch_location' : 'https://www.sqlite.org/chronology.html',
+		'latest_version_re' : "<h1 align=center>History Of SQLite Releases<\/h1>\s+<center>\s+<table border=0 cellspacing=0>\s+<thead>\s+<tr><th>Date<th><th align='left'>Version\s+<\/thead>\s+<tbody>\s+<tr><td><a href='https:\/\/www\.sqlite\.org\/src\/timeline\?c=[0-9a-z]+\&y=ci'>[0-9-]+<\/a><\/td>\s+<td width='20'><\/td>\s+<td><a href=\"releaselog\/[0-9_]+\.html\">([0-9.]+)<\/a><\/td><\/tr>",
+
+		'current_version_fetch_type' : 'hg.moz_re',
+		'current_version_fetch_location': "https://hg.mozilla.org/mozilla-central/raw-file/tip/old-configure.in",
+		'current_version_re': "SQLITE_VERSION=([0-9\.]+)",
+	},
+	{
 		'title' : 'Harfbuzz',
 		'location' : 'gfx/harfbuzz/',
-		'repo_url' : 'https://github.com/behdad/harfbuzz/',
-		'compare_type' : 'release_version',
-		'current_version_file': "https://hg.mozilla.org/mozilla-central/raw-file/tip/gfx/harfbuzz/README-mozilla",
+
+		'latest_version_fetch_type' : 'github_rss',
+		'latest_version_fetch_location' : 'https://github.com/behdad/harfbuzz/',
+
+		'current_version_fetch_type' : 'hg.moz_re',
+		'current_version_fetch_location': "https://hg.mozilla.org/mozilla-central/raw-file/tip/gfx/harfbuzz/README-mozilla",
 		'current_version_re': "Current version:\s*([0-9\.]+)",
 		'ignore' : '1.4.2' #1336500
 	},
 	{
 		'title' : 'Graphite2',
 		'location' : 'gfx/graphite',
-		'compare_type' : 'release_version',
-		'repo_url': "https://github.com/silnrsi/graphite/",
-		'current_version_file': "https://hg.mozilla.org/mozilla-central/raw-file/tip/gfx/graphite2/README.mozilla",
+
+		'latest_version_fetch_type' : 'github_rss',
+		'latest_version_fetch_location': "https://github.com/silnrsi/graphite/",
+
+		'current_version_fetch_type' : 'hg.moz_re',
+		'current_version_fetch_location': "https://hg.mozilla.org/mozilla-central/raw-file/tip/gfx/graphite2/README.mozilla",
 		'current_version_re': "This directory contains the Graphite2 library release ([0-9\.]+) from",
 	},
 	{
 		'title' : 'Hunspell',
 		'location' : 'extensions/spellcheck/hunspell/',
-		'compare_type' : 'release_version',
-		'repo_url' : 'https://github.com/hunspell/hunspell/',
-		'current_version_file': "https://hg.mozilla.org/mozilla-central/raw-file/tip/extensions/spellcheck/hunspell/src/README.mozilla",
+
+		'latest_version_fetch_type' : 'github_rss',
+		'latest_version_fetch_location' : 'https://github.com/hunspell/hunspell/',
+
+		'current_version_fetch_type' : 'hg.moz_re',
+		'current_version_fetch_location': "https://hg.mozilla.org/mozilla-central/raw-file/tip/extensions/spellcheck/hunspell/src/README.mozilla",
 		'current_version_re': "Hunspell Version:\s*v?([0-9\.]+)",
 	},
 	{
 		'title' : 'Codemirror',
 		'location' : 'devtools/client/sourceeditor/codemirror/',
-		'compare_type' : 'release_version',
-		'repo_url' : 'https://github.com/codemirror/CodeMirror/',
-		'current_version_file': "https://dxr.mozilla.org/mozilla-central/source/devtools/client/sourceeditor/codemirror/README",
+
+		'latest_version_fetch_type' : 'github_rss',
+		'latest_version_fetch_location' : 'https://github.com/codemirror/CodeMirror/',
+
+		'current_version_fetch_type' : 'hg.moz_re',
+		'current_version_fetch_location': "https://hg.mozilla.org/mozilla-central/raw-file/tip/devtools/client/sourceeditor/codemirror/README",
 		'current_version_re': "Currently used version is ([0-9\.]+)\. To upgrade",
 	},
 	{
 		'title' : 'pdfjs',
 		'location' : 'browser/extensions/pdfjs',
-		'compare_type' : 'release_version',
 		'allows_ahead' : True,
-		'repo_url' : 'https://github.com/mozilla/pdf.js',
-		'current_version_file': "https://dxr.mozilla.org/mozilla-central/source/browser/extensions/pdfjs/README.mozilla",
+
+		'latest_version_fetch_type' : 'github_rss',
+		'latest_version_fetch_location' : 'https://github.com/mozilla/pdf.js',
+
+		'current_version_fetch_type' : 'hg.moz_re',
+		'current_version_fetch_location': "https://hg.mozilla.org/mozilla-central/raw-file/tip/browser/extensions/pdfjs/README.mozilla",
 		'current_version_re': "Current extension version is: ([0-9\.]+)",
 	},
 	#{
 	#	'title' : 'OTS',
 	#	'location' : 'gfx/ots/',
-	#	'compare_type' : 'git_commit',
 	#	'repo_url' : 'https://github.com/khaledhosny/ots/',
-	#	'current_version_file' : 'https://hg.mozilla.org/mozilla-central/raw-file/tip/gfx/ots/README.mozilla',
+	#	'current_version_fetch_location' : 'https://hg.mozilla.org/mozilla-central/raw-file/tip/gfx/ots/README.mozilla',
 	#	'current_version_re' : 'Current revision:\s*([0-9a-f]+)',
 	#}
 ]
@@ -149,6 +201,8 @@ if __name__ == "__main__":
 		config = l
 		config['verbose'] = verbose
 
+		config = validate_config(config)
+
 		if config['verbose']:
 			print "Examining", config['title'], "(" + config['location'] + ")"
 
@@ -164,7 +218,7 @@ if __name__ == "__main__":
 						print"\tIgnoring outdated version, known bug"
 
 				elif status == AHEAD:
-					if config['allows_ahead']:
+					if 'allows_ahead' in config and config['allows_ahead']:
 						if config['verbose']:
 							print"\tIgnoring ahead version, config allows it"
 					else:
